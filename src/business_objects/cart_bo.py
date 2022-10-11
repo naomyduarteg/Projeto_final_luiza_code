9f0fa5d0ad5c8e9fc51deb9f036e8856778ac1d7
tamaracosta <tamarasantosc@gmail.com>
2022-10-11 09:33:33 +0200

    separando regras de negócio - carrinho na pasta business_objects

---
+++ src/business_objects/cart_bo.py
@@ -0,0 +1,128 @@
+from fastapi import Request, HTTPException, status
+from typing import List
+from fastapi.encoders import jsonable_encoder
+from fastapi import Request
+from typing import List
+
+from src.models.carts import Cart
+from src.models.carts_item import CartsItem
+
+def get_collection_carts(request: Request):
+  return request.app.database["carts"]
+              
+def get_all_carts(request: Request, limit: int):
+  return list(get_collection_carts(request).find(limit=limit))
+
+def get_cart_by_user_id(request: Request, user_id: str):
+  return request.app.database["carts"].find_one({"user_id": user_id})
+
+def calculateTotalPrice(itemList: List[CartsItem]):
+    total = 0
+    
+    for item in itemList:        
+        total += item["quantity"] * item["price"]        
+    return total
+ 
+def addProductItem(item_list: List[CartsItem], item: CartsItem):        
+    is_update = False
+   
+    for cart_item in item_list:
+        if (cart_item["product_id"] == item["product_id"]):
+            cart_item["quantity"] = item["quantity"]
+            is_update = True
+           
+    if (is_update == False):      
+        item_list.append(item)
+           
+    return item_list
+
+def calculateTotalAmount(itemList: List[CartsItem]):
+    total_amount = 0
+   
+    for item in itemList:        
+        total_amount += item["quantity"]
+    return total_amount
+  
+def get_cart(request: Request,user_id: str):
+               
+    if (cart := get_cart_by_user_id(request, user_id)) is not None:
+        return cart
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Cart with user id {user_id} not found")
+  
+def create_cart(request: Request,user_id: str, product_id: str, product_qtt: int):
+    product = request.app.database["items"].find_one({"_id": product_id})    
+    if product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Product with id {product_id} not found")
+   
+    user = request.app.database["users"].find_one({"_id": user_id})    
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {user_id} not found")
+   
+    cart = get_collection_carts(request).find_one({"user_id": user_id})
+    cart_item = CartsItem(product_id=product_id, quantity=product_qtt, price=product["price"])
+   
+    if cart is None:          
+        list_products = [cart_item]        
+        total = product["price"] * product_qtt
+        cart = Cart(user_id= user_id, products= list_products, total_price= total, quantity_products = product_qtt)
+        cart = jsonable_encoder(cart)
+        new_item = get_collection_carts(request).insert_one(cart)
+        created_cart = get_collection_carts(request).find_one(
+            {"_id": new_item.inserted_id}
+        )
+        return created_cart
+   
+    products = cart["products"]
+    addProductItem(products,cart_item)   
+    total = calculateTotalPrice(products)
+    total_amount = calculateTotalAmount(products)    
+    
+    filter = {"_id": cart["_id"]}
+    get_collection_carts(request).update_one(filter, {"$set": {"products": jsonable_encoder(products)}})       
+    get_collection_carts(request).update_one(filter, {"$set": {"total_price": total}})
+    get_collection_carts(request).update_one(filter, {"$set": {"quantity_products": total_amount}})
+        
+    cart["products"] = products
+    cart["total_price"] = total       
+    cart["quantity_products"] = total_amount
+    
+    return cart
+  
+def delete_product(request: Request, user_id: str, product_id: str):
+  cart = get_collection_carts(request).find_one({"user_id": user_id})
+      
+  if cart is None:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Cart with user id {user_id} not found")
+  
+  list_products = cart["products"]
+  product_found = False
+  for product in list_products:
+      if (product["product_id"] == product_id):                
+          list_products.remove(product)
+          product_found = True            
+          cart["total_price"] = calculateTotalPrice(list_products)            
+          cart["quantity_products"] = calculateTotalAmount(list_products)
+          
+  if(product_found == False):
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Product with id {product_id} not found")
+        
+  if list_products == []:
+      delete_cart(request, user_id)                
+      return "Cart with no products. Successfully deleted!" 
+  
+  filter = {"_id": cart["_id"]}
+  get_collection_carts(request).update_one(filter, {"$set": {"products": jsonable_encoder(list_products)}})
+  get_collection_carts(request).update_one(filter, {"$set": {"total_price": cart["total_price"]}})
+  get_collection_carts(request).update_one(filter, {"$set": {"quantity_products": cart["quantity_products"]}})
+  
+  cart["products"] = list_products
+  return cart
+
+def delete_cart(request: Request, user_id: str):
+    cart = get_collection_carts(request).find_one({"user_id": user_id})
+        
+    if cart is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Cart with user id {user_id} not found")
+    
+    get_collection_carts(request).delete_one({"user_id": user_id})
+    return "Cart successfully deleted!"
--- src/endpoints/carts.py
+++ src/endpoints/carts.py
@@ -1,127 +1,27 @@
-from fastapi import APIRouter, Request, HTTPException, status
+from fastapi import APIRouter, Request, status
 from typing import List
-from fastapi.encoders import jsonable_encoder
+
 from src.models.carts import Cart
-from src.models.carts_item import CartsItem
+import src.business_objects.cart_bo as cart_bo
 
 router = APIRouter(prefix="/cart", tags=["Cart"])
 
 @router.get("/",response_description="Get all carts", response_model=List[Cart])
-def return_cart(request: Request):
-    
-    return list(request.app.database["carts"].find(limit=100))
+def get_carts(request: Request):    
+    return cart_bo.get_all_carts(request, 100)    
 
 @router.get("/{user_id}/",response_description="Get a cart by user id",response_model=Cart)
-def return_cart(user_id: str,request: Request):
-               
-    if (cart := request.app.database["carts"].find_one({"user_id": user_id})) is not None:
-        return cart
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Cart with user id {user_id} not found")
+def get_cart(request: Request,user_id: str):               
+    return cart_bo.get_cart(request,user_id)
 
-@router.post("/{user_id}/{product_id}/{product_qtt}", response_description="Create a cart", status_code=status.HTTP_201_CREATED)
-def create_cart(user_id: str, product_id: str, product_qtt: int,request: Request):
-    product = request.app.database["items"].find_one({"_id": product_id})    
-    if product is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Product with id {product_id} not found")
-   
-    user = request.app.database["users"].find_one({"_id": user_id})    
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {user_id} not found")
-   
-    cart = request.app.database["carts"].find_one({"user_id": user_id})
-    cart_item = CartsItem(product_id=product_id, quantity=product_qtt, price=product["price"])
-   
-    if cart is None:          
-        list_products = [cart_item]        
-        total = product["price"] * product_qtt
-        cart = Cart(user_id= user_id, products= list_products, total_price= total, quantity_products = product_qtt)
-        cart = jsonable_encoder(cart)
-        new_item = request.app.database["carts"].insert_one(cart)
-        created_cart = request.app.database["carts"].find_one(
-            {"_id": new_item.inserted_id}
-        )
-        return created_cart
-   
-    products = cart["products"]
-    addProductItem(products,cart_item)   
-    total = calculateTotalPrice(products)
-    total_amount = calculateTotalAmount(products)    
-    
-    filter = {"_id": cart["_id"]}
-    request.app.database["carts"].update_one(filter, {"$set": {"products": jsonable_encoder(products)}})       
-    request.app.database["carts"].update_one(filter, {"$set": {"total_price": total}})
-    request.app.database["carts"].update_one(filter, {"$set": {"quantity_products": total_amount}})
-        
-    cart["products"] = products
-    cart["total_price"] = total       
-    cart["quantity_products"] = total_amount
-    
-    return cart
-
-def calculateTotalPrice(itemList: List[CartsItem]):
-    total = 0
-    
-    for item in itemList:        
-        total += item["quantity"] * item["price"]        
-    return total
- 
-def addProductItem(item_list: List[CartsItem], item: CartsItem):        
-    is_update = False
-   
-    for cart_item in item_list:
-        if (cart_item["product_id"] == item["product_id"]):
-            cart_item["quantity"] = item["quantity"]
-            is_update = True
-           
-    if (is_update == False):      
-        item_list.append(item)
-           
-    return item_list
-
-def calculateTotalAmount(itemList: List[CartsItem]):
-    total_amount = 0
-   
-    for item in itemList:        
-        total_amount += item["quantity"]
-    return total_amount
+@router.post("/{user_id}/{product_id}/{product_qtt}", response_description="Create and update a cart", status_code=status.HTTP_201_CREATED)
+def create_cart(request: Request, user_id: str, product_id: str, product_qtt: int):
+    return cart_bo.create_cart(request, user_id, product_id, product_qtt)
 
 @router.delete("/{user_id}/{product_id}", response_description="Delete an item from cart", status_code=status.HTTP_200_OK)
-def delete_product(user_id: str, product_id: str,request: Request):
-    cart = request.app.database["carts"].find_one({"user_id": user_id})
-        
-    if cart is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Cart with user id {user_id} not found")
-    
-    list_products = cart["products"]
-    product_found = False
-    for product in list_products:
-        if (product["product_id"] == product_id):                
-            list_products.remove(product)
-            product_found = True            
-            cart["total_price"] = calculateTotalPrice(list_products)            
-            cart["quantity_products"] = calculateTotalAmount(list_products)
-            
-    if(product_found == False):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Product with id {product_id} not found")
-         
-    if list_products == []:
-        delete_cart(user_id,request)                
-        return "Cart with no products. Successfully deleted!" 
-    
-    filter = {"_id": cart["_id"]}
-    request.app.database["carts"].update_one(filter, {"$set": {"products": jsonable_encoder(list_products)}})
-    request.app.database["carts"].update_one(filter, {"$set": {"total_price": cart["total_price"]}})
-    request.app.database["carts"].update_one(filter, {"$set": {"quantity_products": cart["quantity_products"]}})
-    
-    cart["products"] = list_products
-    return cart
+def delete_product(request: Request, user_id: str, product_id: str):    
+    return cart_bo.delete_product(request, user_id, product_id)
 
 @router.delete("/{user_id}/", response_description="Delete a cart", status_code=status.HTTP_200_OK)
-def delete_cart(user_id: str, request: Request):
-    cart = request.app.database["carts"].find_one({"user_id": user_id})
-        
-    if cart is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Cart with user id {user_id} not found")
-    
-    request.app.database["carts"].delete_one({"user_id": user_id})
-    return "Cart successfully deleted!"
+def delete_cart(request: Request, user_id: str):    
+    return cart_bo.delete_cart(request, user_id)
