b0f93f8e43c49e1391b9cab3b2e492e98fa7512c
naomyduarteg <naomyduarteg@gmail.com>
2022-10-12 09:51:44 -0300

    comentando a categoria

--- src/models/carts_item_new.py
+++ src/models/carts_item_new.py
@@ -4,7 +4,7 @@ class CartsItemNew(BaseModel):
     user_id: str
     product_id: str
     quantity: int
-    category: str    
+    category: str    #choose Buy or Rent
    
     def __getitem__(self, item):
         return getattr(self, item)
