1d8cd3d6a86a523d68ff394275d1a3ddb55773e8
tamaracosta <tamarasantosc@gmail.com>
2022-10-12 21:25:35 +0200

    mudando request body ao criar carrinho

--- src/models/carts_item_new.py
+++ src/models/carts_item_new.py
@@ -5,6 +5,19 @@ class CartsItemNew(BaseModel):
     product_id: str
     quantity: int
     category: str    #choose Buy or Rent
-   
+      
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "user_id": "982043ee-a5bf-4f43-88ed-6a69523cfbe5",
+                "product_id": "ebbe5ad9-6cb5-4145-9d13-514118382acf",
+                "quantity": 2,
+                "category": "Rent"
+            }
+        }
+    
     def __getitem__(self, item):
-        return getattr(self, item)
+            return getattr(self, item)    
+        
+        
